7a99787fa27e7904d73957fb4c5c5342d08bcdef
PUSH350 <83179457+PUSH350@users.noreply.github.com>
2021-05-03 00:00:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var fixSprite1, fixSprite2, fixSprite3, fixSprite4;
+var movingSprite;
+var canvas;
+var music;
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    movingSprite = createSprite(Random(10,750), 300, 20, 20);
+    movingSprite.shapeColor = "white";
+    movingSprite.velocityX = 3;
+    movingSprite.velocityY = 3;
+
+    fixSprite1 = createSprite(100,590,180,20);
+    fixSprite1.shapeColor = "red";
+
+    fixSprite2 = createSprite(300,590,180,20);
+    fixSprite2.shapeColor = "green";
+
+    fixSprite3 = createSprite(500,590,180,20);
+    fixSprite3.shapeColor = "blue";
+
+    fixSprite4 = createSprite(700,590,180,20);
+    fixSprite4.shapeColor="yellow";
+
+}
+
+function draw() {
+    background(rgb(169,169,169));
+   
+    if(movingSprite.x<0){
+        music.stop()
+        movingSprite.velocityX = 3
+
+    }else if (movingSprite.x>800){
+        music.stop()
+        movingSprite.velocityX = -3
+    }
+
+    if(isTouching(movingSprite, fixSprite4)){
+        music.play()
+        movingSprite.shapeColor = "yellow";
+        bounceOff(movingSprite, fixSprite4)
+    }
+
+    else if(isTouching(movingSprite, fixSprite)){
+        music.stop()
+        movingSprite.shapeColor = "blue";
+        bounceOff(movingSprite, fixSprite3)
+    }
+}
